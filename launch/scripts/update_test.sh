860eeea3e9bf8cff72d528cb5ce5bd31a00b961f
xpullox <greatcg07@outlook.com>
2020-09-12 17:06:46 -0400

    Added new launch feature - uploads script with basic commands no dependancy

--- launch/scripts/update_test.sh
+++ launch/scripts/update_test.sh
@@ -6,6 +6,6 @@ rm -rf client/* && echo ""
 # pause
 sleep 5 && echo ""
 #export new files
-svn export --force --no-auth-cache --username undoLogicReadOnly --password fs8S6mDQNJQHhZ https://@github.com/undologic/projectBrowser/trunk /home/undologic/www/portaltest/client && echo ""
+svn export --force --no-auth-cache --username USERNAME_CHANGE --password PASSWORD_CHANGE https://@github.com/undologic/projectBrowser/trunk /home/undologic/www/portaltest/client && echo ""
 # figure out how to do git export in the future as it is faster
 # git checkout-index -a -f --prefix=/destination/path/
